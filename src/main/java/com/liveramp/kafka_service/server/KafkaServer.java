c9bff789a5cea8107f64da2dc8ca71c812c52aac
Yi Jin <yjin@liveramp.com>
2015-01-22 17:57:36 -0800

    first push, hackweek project

---
+++ src/main/java/com/liveramp/kafka_service/server/KafkaServer.java
@@ -0,0 +1,18 @@
+package com.liveramp.kafka_service.server;
+
+import kafka.server.AbstractFetcherManager;
+
+/**
+ * Created by yjin on 1/22/15.
+ */
+public class KafkaServer {
+
+  private AbstractFetcherManager manager;
+
+  public static void main(String[] args) {
+    KafkaServer server = new KafkaServer();
+
+
+
+  }
+}
---
+++ src/test/java/com/liveramp/kafka_service/server/TestKafkaServer.java
@@ -0,0 +1,7 @@
+package com.liveramp.kafka_service.server;
+
+/**
+ * Created by yjin on 1/22/15.
+ */
+public class TestKafkaServer {
+}
