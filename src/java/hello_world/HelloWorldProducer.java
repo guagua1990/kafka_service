db81a5266c4d0acfde8f07cdf0b3258e4f6d0514
Yi Jin <yjin@liveramp.com>
2015-03-31 15:31:24 -0700

    name packages

--- src/java/hello_world/HelloWorldProducer.java
+++ src/java/hello_world/HelloWorldProducer.java
@@ -10,15 +10,15 @@ import kafka.producer.ProducerConfig;
 public class HelloWorldProducer {
   final static String TOPIC = "hello_world";
 
-  public static void main(String[] argv){
+  public static void main(String[] argv) {
     Properties properties = new Properties();
-    properties.put("metadata.broker.list","localhost:9092");
-    properties.put("serializer.class","kafka.serializer.StringEncoder");
+    properties.put("metadata.broker.list", "localhost:9092");
+    properties.put("serializer.class", "kafka.serializer.StringEncoder");
     ProducerConfig producerConfig = new ProducerConfig(properties);
 
-    kafka.javaapi.producer.Producer<String,String> producer = new kafka.javaapi.producer.Producer<String, String>(producerConfig);
+    kafka.javaapi.producer.Producer<String, String> producer = new kafka.javaapi.producer.Producer<String, String>(producerConfig);
     SimpleDateFormat sdf = new SimpleDateFormat();
-    KeyedMessage<String, String> message =new KeyedMessage<String, String>(TOPIC, "Test message from java program " + sdf.format(new Date()));
+    KeyedMessage<String, String> message = new KeyedMessage<String, String>(TOPIC, "Test message from java program " + sdf.format(new Date()));
     producer.send(message);
     producer.close();
   }
