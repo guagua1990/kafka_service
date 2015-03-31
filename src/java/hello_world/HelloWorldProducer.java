7b7b611e191385b13004b34dad6db789f4384fd4
syan <syan@liveramp.com>
2015-03-31 14:10:01 -0700

    iterative producer

--- src/java/hello_world/HelloWorldProducer.java
+++ src/java/hello_world/HelloWorldProducer.java
@@ -1,5 +1,8 @@
 package hello_world;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Properties;
@@ -16,10 +19,24 @@ public class HelloWorldProducer {
     properties.put("serializer.class","kafka.serializer.StringEncoder");
     ProducerConfig producerConfig = new ProducerConfig(properties);
 
-    kafka.javaapi.producer.Producer<String,String> producer = new kafka.javaapi.producer.Producer<String, String>(producerConfig);
-    SimpleDateFormat sdf = new SimpleDateFormat();
-    KeyedMessage<String, String> message =new KeyedMessage<String, String>(TOPIC, "Test message from java program " + sdf.format(new Date()));
-    producer.send(message);
-    producer.close();
+
+    try {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      String input;
+
+      while((input = br.readLine()) != null){
+        kafka.javaapi.producer.Producer<String,String> producer = new kafka.javaapi.producer.Producer<String, String>(producerConfig);
+        SimpleDateFormat sdf = new SimpleDateFormat();
+        KeyedMessage<String, String> message =new KeyedMessage<String, String>(TOPIC, "syan: " + input + " " + sdf.format(new Date()));
+        producer.send(message);
+        producer.close();
+      }
+
+    } catch(IOException io){
+      io.printStackTrace();
+    }
+
+
   }
 }
