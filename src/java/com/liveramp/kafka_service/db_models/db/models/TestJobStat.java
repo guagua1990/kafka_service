56a31949492c8cf0d46ac03462c203b2498147c4
LiRen Tu <ltu@liveramp.com>
2015-04-02 18:18:25 -0700

    fix yml

--- src/java/com/liveramp/kafka_service/db_models/db/models/TestJobStat.java
+++ src/java/com/liveramp/kafka_service/db_models/db/models/TestJobStat.java
@@ -5,16 +5,14 @@ import org.junit.Test;
 import com.liveramp.kafka_service.db_models.IDatabases;
 import com.liveramp.kafka_service.db_models.db.iface.IJobStatPersistence;
 import com.liveramp.kafka_service.db_models.DatabasesImpl;
-import com.rapleaf.jack.DatabaseConnection;
 
 public class TestJobStat {
-  private static final DatabaseConnection CONN = new DatabaseConnection("database");
-  private static final IDatabases dbs = new DatabasesImpl(CONN);
+  private static final IDatabases dbs = new DatabasesImpl();
 
   private final IJobStatPersistence jobStats = dbs.getKafkaService().jobStats();
 
   @Test
   public void testJobStatCreation() throws Exception {
-    jobStats.createDefaultInstance();
+    jobStats.deleteAll();
   }
 }
